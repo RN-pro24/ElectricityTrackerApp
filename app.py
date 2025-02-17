1e4feeb25ec9912b861bf6ba5775d0528b1657db
RN-pro24 <renynaranjo@outlook.com>
2025-02-17 03:03:37 +0000

    Actualizacion del helpers

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import base64
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import mysql.connector
+import helpers
 
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
@@ -24,16 +24,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure MySQL database
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="admin_user",
-  password="Isa240122-*",
-  database="electricity_tracker_app"
-)
-
-mycursor = mydb.cursor()
-
 # Configura la fecha
 current_date = datetime.now().date()
 
@@ -50,5 +40,63 @@ def after_request(response):
 @app.route("/", methods=["GET", "POST"])
 def home():
     return render_template('index.html')
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    errors = {}
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            errors["username"] = "must provide username"
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            errors["password"] = "must provide password",
+
+        # Query database for username
+        user_data = helpers.query_db(
+            "SELECT * FROM users WHERE username = %s", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(user_data) != 1 or not check_password_hash(
+            user_data[0]["hash"], request.form.get("password")
+        ):
+            errors["access"] = "invalid username and/or password"
+
+        if errors:
+            return render_template("login.html", errors=errors)
+
+        # Remember which user has logged in
+        session["user_id"] = user_data[0]["id"]
+
+        # Redirect user to home page
+        use = "This form, and others like it, let you customize where you want to record your money. Under 'asset,' choose an existing account or create a new one. It's the same for all categories. For example, use 'receivable' for pending income and 'savings' for, well, savings!"
+        flash(use)
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html", errors=errors)
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
 
 
--- helpers.py
+++ helpers.py
@@ -1,1 +1,29 @@
-#funciones auxiliares
+import mysql.connector
+
+#Conect with mySQL database
+def connect_db():
+    return mysql.connector.connect(
+        host="localhost",
+        user="admin_user",
+        password="Isa240122-*",
+        database="electricity_tracker_app"
+    )
+
+#make query in database
+def query_db(query, parameters=None):
+    mydb = connect_db()
+    mycursor = mydb.cursor(dictionary=True)
+    mycursor.execute(query, parameters)
+    results = mycursor.fetchall()
+    mycursor.close()
+    mydb.close()
+    return results
+
+#insert data en database
+def insert_db(query, data):
+    mydb = connect_db()
+    mycursor = mydb.cursor()
+    mycursor.execute(query, data)
+    mydb.commit()
+    mycursor.close()
+    mydb.close()
