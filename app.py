39e20ac10c683a37c5707fdfef13b1380c30ddac
RN-pro24 <renynaranjo@outlook.com>
2025-02-22 03:14:57 +0000

    Actualizacion de la app

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import helpers
 
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 from io import BytesIO
@@ -15,6 +17,11 @@ from io import BytesIO
 
 # Configure application
 app = Flask(__name__)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["5 per minute", "30 per hour"]
+)
 
 # Custom filter
 app.jinja_env.filters["usd"] = 1
@@ -36,57 +43,80 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-#
+#Home page
 @app.route("/", methods=["GET", "POST"])
 def home():
     return render_template('index.html')
 
+#Register new user
+@app.route("/register", methods=["GET","POST"])
+def register():
+    """"Register user"""
+
+    session.clear()
+    errors = {}
+
+    if request.method == "POST":
+        errors = helpers.validate_new_user_data(request.form)
+
+        if not errors:
+            #Get all user data
+            first_name = request.form.get("first_name")
+            last_name = request.form.get("last_name")
+            email = request.form.get("email")
+            user_name = request.form.get("user_name")
+            password= request.form.get("password")
+
+            hashed_password = generate_password_hash(password)
+
+            helpers.insert_db("INSERT INTO users(first_name, last_name, email, user_name, password) VALUES (%s,%s,%s,%s,%s)",
+                               first_name, last_name, email, user_name,hashed_password
+                              )
+            
+            user_data = helpers.query_db(
+                    "SELECT * FROM users WHERE user_name = %s", user_name
+                )
+        
+            session["user_id"] = user_data[0]["id"]
+            flash("Registration successful")
+            return redirect("/")
+        
+        else:
+            return render_template("register.html", errors= errors)
+    else:
+        return render_template("register.html", errors={})
 
 
 @app.route("/login", methods=["GET", "POST"])
+@limiter.limit("5 per minute")
 def login():
     """Log user in"""
 
     # Forget any user_id
     session.clear()
 
-    errors = {}
-
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            errors["username"] = "must provide username"
+        #Validate user entry
+        errors, user_data = helpers.validate_user_data(request.form)
 
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            errors["password"] = "must provide password",
+        #if not errors proceed with login
+        if not errors:
+            # Remember which user has logged in
+            session["user_id"] = user_data[0]["id"]
 
-        # Query database for username
-        user_data = helpers.query_db(
-            "SELECT * FROM users WHERE username = %s", request.form.get("username")
-        )
+            # Redirect user to home page
+            use = "This module allows you to observe your currents metrics"
+            flash(use)
+            return redirect("/")
 
-        # Ensure username exists and password is correct
-        if len(user_data) != 1 or not check_password_hash(
-            user_data[0]["hash"], request.form.get("password")
-        ):
-            errors["access"] = "invalid username and/or password"
-
-        if errors:
+        #if errors, re-render login page with errors    
+        else:
             return render_template("login.html", errors=errors)
-
-        # Remember which user has logged in
-        session["user_id"] = user_data[0]["id"]
-
-        # Redirect user to home page
-        use = "This form, and others like it, let you customize where you want to record your money. Under 'asset,' choose an existing account or create a new one. It's the same for all categories. For example, use 'receivable' for pending income and 'savings' for, well, savings!"
-        flash(use)
-        return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("login.html", errors=errors)
+        return render_template("login.html", errors={})
 
 
 @app.route("/logout")
--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from werkzeug.security import check_password_hash, generate_password_hash
 
 #Conect with mySQL database
 def connect_db():
@@ -27,3 +28,70 @@ def insert_db(query, data):
     mydb.commit()
     mycursor.close()
     mydb.close()
+
+#Validation for new users
+
+def validate_new_user_data(form_data):
+
+    #Create dictionary for errors
+    errors = {}
+
+    #Validate all the input entry
+    if not form_data.get("first_name"):
+        errors["first_name"] = "Must provide first name"
+    if not form_data.get("last_name"):
+        errors["last_name"] = "Must provide last name"
+    if not form_data.get("email"):
+        errors["email"] = "Must provide email"
+    if not form_data.get("user_name"):
+        errors["user_name"] = "Must provide username"
+    if not form_data.get("password"):
+        errors["password"] = "Must provide password"
+    if not form_data.get("confirmation"):
+        errors["confirmation"] = "Must confirm password"
+    
+    if errors:
+        return errors
+    
+    #Validate username and passwords
+
+    request_user = form_data.get("user_name")
+    try:
+        if query_db("SELECT * FROM users WHERE user_name = %s", request_user):
+            errors["user_exist"] = "The username exists"
+    except Exception as e:
+            errors["database_error"] = f"An error occurred: {e}"
+    
+    if form_data.get("password") != form_data.get("confirmation"):
+        errors["match"] = "passwords don't macht"
+    
+    return errors if errors else None
+
+def validate_user_data(form_data):
+    
+    #Create dictionary for errors
+    errors = {}
+        
+    # Ensure username and password was submitted
+    if not form_data.get("username"):
+        errors["username"] = "must provide username"
+    if not form_data.get("password"):
+        errors["password"] = "must provide password"
+    
+    if errors:
+        return errors, None
+    
+    # Query database for username
+    user_data = query_db(
+        "SELECT * FROM users WHERE user_name = %s", form_data.get("username")
+    )
+
+    # Ensure username exists and password is correct
+    if len(user_data) != 1 or not check_password_hash(
+        user_data[0]["password"], form_data.get("password")
+    ):
+        errors["access"] = "invalid username and/or password"
+        return errors, None
+    
+    else:
+        return errors, user_data
